be191f11560aca23dbbf541511049da4e30266b5
Jesus-4Geeks <madafaking@PC-JML>
2025-04-11 14:53:22 +0200

    Uso de ternarias

--- src/js/app.js
+++ src/js/app.js
@@ -33,14 +33,19 @@ function render(variables = {}) {
   document.querySelector("#widget_content").innerHTML = `<div class="widget">
             ${cover}
           <img src="${variables.avatarURL}" class="photo" />
-          <h1>${variables.name} ${variables.lastName}</h1>
-          <h2>${variables.role}</h2>
-          <h3>${variables.city}, ${variables.country}</h3>
-          <ul class="${variables.socialMediaPosition}">
-            <li><a href="https://twitter.com/${variables.twitter}"><i class="fab fa-twitter"></i></a></li>
-            <li><a href="https://github.com/${variables.github}"><i class="fab fa-github"></i></a></li>
-            <li><a href="https://linkedin.com/school/${variables.linkedin}"><i class="fab fa-linkedin"></i></a></li>
-            <li><a href="https://instagram.com/${variables.instagram}"><i class="fab fa-instagram"></i></a></li>
+          <h1>${variables.name || "Lucy"} ${variables.lastName ||
+    "Boilett"}</h1>
+          <h2>${variables.role || "Web Developer"}</h2>
+          <h3>${variables.city || "Miami"}, ${variables.country || "USA"}</h3>
+          <ul class="${variables.socialMediaPosition || "position-right"}">
+            <li><a href="https://twitter.com/${variables.twitter ||
+              "4geeksacademy"}"><i class="fab fa-twitter"></i></a></li>
+            <li><a href="https://github.com/${variables.github ||
+              "4geeksacademy"}"><i class="fab fa-github"></i></a></li>
+            <li><a href="https://linkedin.com/school/${variables.linkedin ||
+              "4geeksacademy"}"><i class="fab fa-linkedin"></i></a></li>
+            <li><a href="https://instagram.com/${variables.instagram ||
+              "4geeksacademy"}"><i class="fab fa-instagram"></i></a></li>
           </ul>
         </div>
     `;
@@ -60,15 +65,15 @@ window.onload = function() {
     // social media bar position (left or right)
     socialMediaPosition: "position-left",
     // social media usernames
-    twitter: "4geeksacademy",
-    github: "4geeksacademy",
-    linkedin: "4geeksacademy",
-    instagram: "4geeksacademy",
-    name: "Lucy",
-    lastName: "Boilett",
-    role: "Web Developer",
-    country: "USA",
-    city: "Miami"
+    twitter: null,
+    github: null,
+    linkedin: null,
+    instagram: null,
+    name: null,
+    lastName: null,
+    role: null,
+    country: null,
+    city: null
   };
   render(window.variables); // render the card for the first time
 
